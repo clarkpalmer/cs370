0dc6fa20ebcbdb4bf58414d43a3f85cf33ded73c
Your Name <ashvin.nihalani@gmail.com>
2019-12-13 00:03:27 -0800

    Heroku Bug v2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { ENV['MAIL_HOST'] }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
     :port           => '587',
